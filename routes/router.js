12ecc4bb1dbb2ddb133a382ddc6216b846e8fd51
Israr Kboubi <kouiisrar@gmail.com>
2024-03-06 01:10:41 +0100

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -2,12 +2,14 @@ const routes = require("express").Router();
 const articles = require("./articles");
 const auth = require("./auth");
 
-routes.use("/", articles);
-routes.use("/auth", auth);
 routes.get("/", async function (req, res) {
     res.send(`<h1>Reached home!</h1> 
               <br>
               <a href='/books'>Books</a>`);
   });
+  
+routes.use("/", articles);
+routes.use("/auth", auth);
+
 console.log(routes)
 module.exports = routes;
