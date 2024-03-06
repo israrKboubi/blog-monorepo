3492f134998b1d301c21e1010e351d0545483e4c
Israr Kboubi <kouiisrar@gmail.com>
2024-03-06 01:21:29 +0100

    update

--- api/articles.js
+++ api/articles.js
@@ -1,6 +1,9 @@
 const app = require("../app");
 const route = require("../routes/articles");
+const route = require("../routes/auth");
+
 
 app.use("/api/", route);
+app.use("/auth/", route);
 
 module.exports = app;
--- routes/router.js
+++ routes/router.js
@@ -2,14 +2,9 @@ const routes = require("express").Router();
 const articles = require("./articles");
 const auth = require("./auth");
 
-routes.get("/", async function (req, res) {
-    res.send(`<h1>Reached home!</h1> 
-              <br>
-              <a href='/books'>Books</a>`);
-  });
 
-//routes.use("/", articles);
-routes.use("/", auth);
+routes.use("/", articles);
+//routes.use("/", auth);
 
 console.log(routes)
 module.exports = routes;
